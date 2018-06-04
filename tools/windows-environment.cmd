a13b5bf61c0d21f8edc57d12d4f905a04d75ca2f
Lcnvdl <programando.soft@gmail.com>
2018-06-04 10:27:13 -0300

    Estructura

--- tools/windows-environment.cmd
+++ tools/windows-environment.cmd
@@ -1,3 +1,4 @@
 cd ..\monstruos
+copy /Y makefiles\Makefile.windows Makefile
 cls
 @%comspec% /K "path %PROGRAMFILES%\NASM;%PROGRAMFILES%\qemu;%USERPROFILE%\.monstruos\tools\i386-elf-gcc\bin;%USERPROFILE%\.monstruos\tools\i386-elf-binutils\bin;%PATH% && cls && @echo MONSTRUOS BUILD ENVIRONMENT"
